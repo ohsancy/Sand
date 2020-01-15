ec7b661c650dba1cf526106496f8bb6e89607bd1
Josh <josh.sancuo@gmail.com>
2020-01-15 17:52:08 +0800

    add terms index ui

---
+++ lib/terms/ui/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Index extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _Index();
+}
+
+class _Index extends State<Index> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
